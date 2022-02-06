94f4ad0f57d0edf4755fe091c79b17f50ec303dc
bruce <bruceliu1107@gmail.com>
2022-02-06 15:35:47 -0500

    buffer class

--- src/simulation.py
+++ src/simulation.py
@@ -1,5 +1,6 @@
 from timeGenerator import TimeGenerator
 import numpy as np
+from random import seed, random
 class simulation():
     def __init__(self):
         self.part1Target = 10
@@ -17,9 +18,20 @@ class simulation():
         self.bufferC3W3 = 0
     def sim(self):
         timeGenerator = TimeGenerator()
+        seed(1)
         while(self.p1Made < self.part1Target or self.p2Made < self.part2Target or self.p3Made < self.part3Target):
-            inspector1Time = timeGenerator.getIns1()
-            print(inspector1Time)
+            inspector1C1 = timeGenerator.getIns1() 
+            
+            rand = random()
+            if(rand>0.5):
+                print("Inspector 2 generate C2")
+                inspector2C2 = timeGenerator.getIns22()
+            else:
+                print("Inspector 2 generate C2")
+                inspector2C3 = timeGenerator.getIns23()
+            
+
+            
 
 
 sim = simulation()
