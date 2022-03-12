2cde47958b6a6d0d67f9622cd344615c74bbde44
bruce <bruceliu1107@gmail.com>
2022-03-11 21:04:44 -0500

    time generator 2nd edition

--- src/timeGenerator.py
+++ src/timeGenerator.py
@@ -2,7 +2,6 @@ import random
 import numpy
 
 class timeGenerator:
-    MLE_ins1 = 0.096545
     def __init__(self,seed) -> None:
         random.seed(seed)
     
@@ -12,5 +11,42 @@ class timeGenerator:
         MLE = 0.096545
         ins1_time = numpy.log(1-p) / (-MLE)
         return ins1_time    
+    
+    def getIns2Time(self):
+        p = random.random()
+        print(p)
+        MLE = 0.064362
+        ins1_time = numpy.log(1-p) / (-MLE)
+        return ins1_time  
+    
+    def getIns3Time(self):
+        p = random.random()
+        print(p)
+        MLE = 0.048466621
+        ins1_time = numpy.log(1-p) / (-MLE)
+        return ins1_time  
+
+    def getWs1Time(self):
+        p = random.random()
+        print(p)
+        MLE = 0.217182777
+        ins1_time = numpy.log(1-p) / (-MLE)
+        return ins1_time  
+    
+    def getWs2Time(self):
+        p = random.random()
+        print(p)
+        MLE = 0.090150136
+
+        ins1_time = numpy.log(1-p) / (-MLE)
+        return ins1_time  
+    
+    def getWs3Time(self):
+        p = random.random()
+        print(p)
+        MLE = 0.113693469
+
+        ins1_time = numpy.log(1-p) / (-MLE)
+        return ins1_time  
 
 
